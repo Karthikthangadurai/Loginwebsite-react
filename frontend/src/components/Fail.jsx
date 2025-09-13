9a93a412ae1462a4281908751f797ceff3ad97e3
Karthik T <presikarthik@gmail.com>
2025-09-13 19:21:36 +0530

    Update Fail.jsx

--- frontend/src/components/Fail.jsx
+++ frontend/src/components/Fail.jsx
@@ -8,7 +8,7 @@ const Fail = () => {
         <h1>LOGIN FAILED</h1>
         <h2 className="text-yellow-500">Sorry😔,Incorrect mail id, password.</h2>
         <p>Please Enter Correct mail id, password✅</p>
-        <form action="http://localhost:5173/">
+        <form action="https://loginwebsite-react-frontend.onrender.com">
         <button className="bg-green-200 text-purple-700 font-semibold mt-6 p-2 border rounded-lg">🔙 Go Home</button>
         </form>
     </div>
